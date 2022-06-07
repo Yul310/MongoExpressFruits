0b890eccad8bc12973c02fa864f14d324e9be5a1
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-06-07 11:16:06 -0400

    2nd update

--- server.js
+++ server.js
@@ -3,17 +3,11 @@
 /////////////////////////////////////////////
 require("dotenv").config(); // Load ENV Variables
 const express = require("express"); // import express
-const morgan = rquire("morgan");
+const morgan = require("morgan");
 const methodOverride = require("method-override")
-const req =require("express/lib/request")
+// const req =require("express/lib/request")
 const mongoose = require("mongoose")
 const path = require("path")
-
-
-
-
-
-
 
 
 
@@ -21,5 +15,42 @@ const path = require("path")
 // Database Connection
 /////////////////////////////////////////////
 
+// Setup inputs for our connect function
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 
+//Establish Connection
+mongoose.connect(DATABASE_URL,CONFIG);
+
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+
+// pull schema and model from mongoose
+
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+//is equal to:
+const {Schema,model} = mongoose
+
+//Make fruits Schema
+const fruitsSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean,
+});
+
+const Fruit = model("Fruit",fruitsSchema)
