b4e5a789717b035a05c3f1c62d7597d93580c7ff
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-06-07 10:22:34 -0400

    initiation

---
+++ server.js
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = rquire("morgan");
+const methodOverride = require("method-override")
+const req =require("express/lib/request")
+const mongoose = require("mongoose")
+const path = require("path")
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+
+
+
